0b2d8128e41eab85399ffc384de63ef75ec13678
Matt Bateman <mbatem1882@yahoo.com>
2020-04-06 01:12:07 -0400

    first commit

---
+++ StoreInventoryManagement/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace StoreInventoryManagement.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
