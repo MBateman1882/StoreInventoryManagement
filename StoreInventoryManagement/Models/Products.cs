e3bdde7971f214cef1f647544b618fd2b2ed8db0
Matt Bateman <mbatem1882@yahoo.com>
2020-04-09 01:23:19 -0400

    Connected to local database.

---
+++ StoreInventoryManagement/Models/Products.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace StoreInventoryManagement.Models
+{
+    public partial class Products
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; }
+        public string Department { get; set; }
+        public decimal Price { get; set; }
+        public int BalanceOnHand { get; set; }
+        public int Minimum { get; set; }
+        public int Allocation { get; set; }
+        public int AmountOrdered { get; set; }
+        public DateTime? NextExpirationDate { get; set; }
+        public int DaySales { get; set; }
+        public int WeekSales { get; set; }
+        public int MonthSales { get; set; }
+    }
+}
---
+++ StoreInventoryManagement/Models/StoreInventoryManagementContext.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace StoreInventoryManagement.Models
+{
+    public partial class StoreInventoryManagementContext : DbContext
+    {
+        public StoreInventoryManagementContext()
+        {
+        }
+
+        public StoreInventoryManagementContext(DbContextOptions<StoreInventoryManagementContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Products> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=.\\sqlexpress;Database=StoreInventoryManagement;Integrated Security=True");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Products>(entity =>
+            {
+                entity.Property(e => e.Department).IsRequired();
+
+                entity.Property(e => e.ItemName).IsRequired();
+
+                entity.Property(e => e.NextExpirationDate).HasColumnType("datetime");
+
+                entity.Property(e => e.Price).HasColumnType("money");
+            });
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
